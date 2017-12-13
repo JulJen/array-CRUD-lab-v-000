fbe8a87f242def7cb74a220c9e57e779fdc292ef
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-13 20:45:11 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,6 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   array = ["red", "orange", "yellow", "green"]
   array << "blue"
+  puts array.inspect
 end
 
 def add_element_to_start_of_array(array, element)
